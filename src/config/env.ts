953e79737e102222e0326c9d914461680c11c9c7
minhtungo <loducfc@gmail.com>
2025-02-15 17:59:49 -0500

    chore: add drizzle

---
+++ src/config/env.ts
@@ -0,0 +1,8 @@
+import dotenv from 'dotenv';
+import { cleanEnv, host, num, port, str, testOnly } from 'envalid';
+
+dotenv.config();
+
+export const env = cleanEnv(process.env, {
+  DATABASE_URL: str(),
+});
